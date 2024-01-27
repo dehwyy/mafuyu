293505be880f09c3b0f6f8f6646e694cdb78c372
dehwyy <dehwyy@yandex.ru>
2024-01-28 01:05:25 +0300

    [libs/utils] Errors - rework file

--- libs/utils/src/errors.rs
+++ libs/utils/src/errors.rs
@@ -1,13 +1,59 @@
 use tonic::Status;
+use crate::errors::prelude::RepositoryError;
 
+/// ### HandleError Trait
 pub trait HandleError<T, R> {
   fn handle(self) -> Result<T, R>;
 }
 
+impl<T> HandleError<T, Status> for Result<T, RepositoryError> {
+  fn handle(self) -> Result<T, Status> {
+    match self {
+      Ok(model) => Ok(model),
+      Err(err) => {
+        match err {
+          RepositoryError::DbError(err) => Err(Status::internal(err)),
+          RepositoryError::NotFound(err) => Err(Status::not_found(err))
+        }
+      }
+    }
+  }
+}
+
+impl<T> HandleError<T, Status> for Result<T, async_nats::RequestError> {
+  fn handle(self) -> Result<T, Status> {
+    match self {
+      Ok(v) => Ok(v),
+      Err(err) => Err(Status::internal(err.to_string()))
+    }
+  }
+}
+
+impl<T> HandleError<T, Status> for serde_json::Result<T> {
+  fn handle(self) -> Result<T, Status> {
+    match self {
+      Ok(v) => Ok(v),
+      Err(err) =>Err(Status::internal(err.to_string()))
+    }
+  }
+}
+
+
+/// ### SafeUnwrap Trait
 pub trait SafeUnwrapWithMessage<T, R> {
   fn safe_unwrap(self, msg: &str) -> Result<T, R>;
 }
 
+impl<T> SafeUnwrapWithMessage<T, Status> for Option<T> {
+  fn safe_unwrap(self, msg: &str) -> Result<T, Status> {
+    match self {
+      Some(v) => Ok(v),
+      None => Err(Status::internal(msg))
+    }
+  }
+}
+
+/// ### GrpcHandleError Trait
 pub trait GrpcHandleError<T> {
   fn internal_error(self) -> Result<T, Status>;
   fn invalid_argument_error(self) -> Result<T, Status>;
@@ -17,10 +63,43 @@ pub trait GrpcHandleError<T> {
   fn unauthenticated_error(self) -> Result<T, Status>;
 }
 
+impl<T> GrpcHandleError<T> for Result<T, String> {
+  fn internal_error(self) -> Result<T, Status> {
+    self.map_err(|err| Status::internal(err))
+  }
+  fn invalid_argument_error(self) -> Result<T, Status> {
+    self.map_err(|err| Status::invalid_argument(err))
+  }
+  fn not_found_error(self) -> Result<T, Status> {
+    self.map_err(|err| Status::not_found(err))
+  }
+  fn already_exists_error(self) -> Result<T, Status> {
+    self.map_err(|err| Status::already_exists(err))
+  }
+  fn permission_denied_error(self) -> Result<T, Status> {
+    self.map_err(|err| Status::permission_denied(err))
+  }
+  fn unauthenticated_error(self) -> Result<T, Status> {
+    self.map_err(|err| Status::unauthenticated(err))
+  }
+}
+
+/// ### HandleRepositoryError Trait
+// * Extracts `Value` from `Result` or returns boilerplate `tonic::Status` based on kind of `RepositoryError`
 pub trait HandleRepositoryError<T> {
   fn is_not_found(&self) -> bool;
 }
 
+impl<T> HandleRepositoryError<T> for Result<T, RepositoryError> {
+  fn is_not_found(&self) -> bool {
+    if let Err(err) = &self {
+      if let RepositoryError::NotFound(err) = err {
+        return true;
+      }
+    }
+    false
+  }
+}
 
 mod repository {
   use std::fmt::Formatter;
@@ -64,73 +143,6 @@ mod repository {
       }
     }
 
-  }
-}
-
-mod service {
-  use tonic::Status;
-  use super::*;
-
-  pub mod prelude {
-    use super::*;
-    use super::repository::RepositoryError;
-
-
-    impl<T> GrpcHandleError<T> for Result<T, String> {
-      fn internal_error(self) -> Result<T, Status> {
-        self.map_err(|err| Status::internal(err))
-      }
-      fn invalid_argument_error(self) -> Result<T, Status> {
-        self.map_err(|err| Status::invalid_argument(err))
-      }
-      fn not_found_error(self) -> Result<T, Status> {
-        self.map_err(|err| Status::not_found(err))
-      }
-      fn already_exists_error(self) -> Result<T, Status> {
-        self.map_err(|err| Status::already_exists(err))
-      }
-      fn permission_denied_error(self) -> Result<T, Status> {
-        self.map_err(|err| Status::permission_denied(err))
-      }
-      fn unauthenticated_error(self) -> Result<T, Status> {
-        self.map_err(|err| Status::unauthenticated(err))
-      }
-    }
-
-    impl<T> SafeUnwrapWithMessage<T, Status> for Option<T> {
-      fn safe_unwrap(self, msg: &str) -> Result<T, Status> {
-        match self {
-          Some(v) => Ok(v),
-          None => Err(Status::internal(msg))
-        }
-      }
-    }
-
-    impl<T> HandleError<T, Status> for Result<T, RepositoryError> {
-      fn handle(self) -> Result<T, Status> {
-        match self {
-          Ok(model) => Ok(model),
-          Err(err) => {
-            match err {
-              RepositoryError::DbError(err) => Err(Status::internal(err)),
-              RepositoryError::NotFound(err) => Err(Status::not_found(err))
-            }
-          }
-        }
-      }
-    }
-
-    // Extracts `Value` from `Result` or returns boilerplate `tonic::Status` based on kind of `RepositoryError`
-    impl<T> HandleRepositoryError<T> for Result<T, RepositoryError> {
-      fn is_not_found(&self) -> bool {
-        if let Err(err) = &self {
-          if let RepositoryError::NotFound(err) = err {
-            return true;
-          }
-        }
-        false
-      }
-    }
   }
 }
 
