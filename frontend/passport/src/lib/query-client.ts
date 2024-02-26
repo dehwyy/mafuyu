be90be03dcb35cfbd22d2cac6958d80424db1809
dehwyy <dehwyy@yandex.ru>
2024-02-26 22:29:27 +0300

    [queryClient] try to refresh token on error

--- frontend/passport/src/lib/query-client.ts
+++ frontend/passport/src/lib/query-client.ts
@@ -1,24 +1,26 @@
-import { QueryClient } from "@tanstack/svelte-query"
+import { QueryClient, QueryCache } from "@tanstack/svelte-query"
 import { ParseCookie } from "$lib/utils/parse-cookie"
 import { GrpcCookiesKeys, GrpcErrors } from "@makoto/grpc/const"
 import { GrpcWebClient, type FinishedUnaryCall, type RpcMetadata, RpcError } from "@makoto/grpc/web"
 import { browser } from "$app/environment"
 
 export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: async (error, _) => {
+      if (error instanceof RpcError) {
+        const refresh_token = ParseCookie(document.cookie)[GrpcCookiesKeys.RefreshToken]
+        if (error.code === GrpcErrors.UNAUTHENTICATED && refresh_token) {
+          // Try to refresh token
+          await GrpcWebClient.refreshTheToken({
+            refreshToken: refresh_token,
+          })
+        }
+      }
+    },
+  }),
   defaultOptions: {
     queries: {
       enabled: browser,
-      // async onError(error) {
-      //   if (error instanceof RpcError) {
-      //     const refresh_token = ParseCookie(document.cookie)[GrpcCookiesKeys.RefreshToken]
-      //     if (error.code === GrpcErrors.UNAUTHENTICATED && refresh_token) {
-      //       // Try to refresh token
-      //       await GrpcWebClient.refreshTheToken({
-      //         refreshToken: refresh_token,
-      //       })
-      //     }
-      //   }
-      // },
     },
   },
 })
