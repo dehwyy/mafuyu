db41f669c2c12a792f0a562eeb902e1a189fa5f2
dehwyy <dehwyy@yandex.ru>
2024-03-02 10:17:14 +0300

    [logger] Rust - tracing for Sentry

--- libs/logger/src/lib.rs
+++ libs/logger/src/lib.rs
@@ -1,5 +1,9 @@
+use std::fmt::Debug;
+use tracing_subscriber::prelude::*;
 use tracing_subscriber::fmt::time::ChronoLocal;
-pub use tracing::{trace, debug, info, error, warn};
+use tracing::instrument::WithSubscriber;
+
+pub use tracing::{trace, debug, info, error, warn, instrument};
 
 #[derive(Clone)]
 enum  LoggerMode {
@@ -37,17 +41,18 @@ impl Logger {
         let mode = LoggerMode::new(environment);
         let level: tracing::Level = mode.clone().into();
 
-        let base = tracing_subscriber::FmtSubscriber::builder()
+        let fmt_subscriber = tracing_subscriber::FmtSubscriber::builder()
             .with_target(true)
             .with_file(true)
             .with_line_number(true)
             .with_ansi(true)
+            .pretty()
             .with_timer(ChronoLocal::new("%Y-%m-%d %H:%M:%S".to_owned()))
-            .with_max_level(level);
+            .with_max_level(level)
+            .finish();
 
-        match mode {
-            LoggerMode::Production => tracing::subscriber::set_global_default(base.compact().finish()).unwrap(),
-            _ => tracing::subscriber::set_global_default(base.pretty().finish()).unwrap()
-        }
+        fmt_subscriber
+            .with(sentry::integrations::tracing::layer())
+            .init();
     }
 }
--- libs/sentry/src/lib.rs
+++ libs/sentry/src/lib.rs
@@ -3,6 +3,7 @@ pub struct Sentry;
 impl Sentry {
     pub fn new(dsn: String) -> sentry::ClientInitGuard {
         let sentry = sentry::init((dsn, sentry::ClientOptions{
+            release: sentry::release_name!(),
             traces_sample_rate: 1.0,
             sample_rate: 1.0,
             debug: true,
