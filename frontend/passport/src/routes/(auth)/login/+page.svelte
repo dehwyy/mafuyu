f7aa94b73234906260f99cfcf81fc738ee12eb7f
dehwyy <dehwyy@yandex.ru>
2024-07-05 12:04:13 +0300

    [frontend|fix] Passport - fallback for `access denied` in google oauth2 when click `cancel`

--- frontend/passport/src/routes/(auth)/login/+page.svelte
+++ frontend/passport/src/routes/(auth)/login/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+  import { page } from '$app/stores'
   import LoginForm from '$lib/components/form/login.svelte'
   import OAuthForm from '$lib/components/form/oauth.svelte'
+  import { Toasts } from '$lib/utils/toast'
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    const err = $page.url.searchParams.get('error')
+    const errorDescription = $page.url.searchParams.get('error_desc')
+    err && Toasts.error(errorDescription)
+  })
 </script>
 
 <LoginForm />
--- frontend/passport/src/routes/api/v1/oauth/google/callback/+server.ts
+++ frontend/passport/src/routes/api/v1/oauth/google/callback/+server.ts
@@ -2,14 +2,17 @@ import { GrpcClient, Interceptors } from '@makoto/grpc'
 import { redirect } from '@sveltejs/kit'
 import type { RequestHandler } from '@sveltejs/kit'
 
+
+
+
+
 export const GET: RequestHandler = async ({ url, cookies, setHeaders }) => {
   const code = url.searchParams.get('code')
   const csrfToken = url.searchParams.get('state')
 
-  if (code === null || csrfToken === null)
-    new Response(null, {
-      status: 400
-    })
+  if (code === null || csrfToken === null) {
+    redirect(307, `/login?error=access_denied&error_desc=Access%20denied`)
+  }
 
   console.log(code, csrfToken)
   const { response, headers } = await GrpcClient.signInOAuth2(
